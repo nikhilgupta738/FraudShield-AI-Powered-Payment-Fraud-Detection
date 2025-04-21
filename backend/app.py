d3c08b711eecdb86c096e7f3f7c68729d009d038
nikhilgupta738 <ng929439@gmail.com>
2025-04-21 23:57:36 +0530

    added backend and connect it to frontend

--- backend/app.py
+++ backend/app.py
@@ -7,17 +7,20 @@ app = Flask(__name__, template_folder='../frontend')
 # Load the model
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
-
+    
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()  # receive data like {"features": [val1, val2, ...]}
-    features = np.array(data['features']).reshape(1, -1)
-    prediction = model.predict(features)
-    return jsonify({'prediction': int(prediction[0])})
+    try:
+        data = request.get_json()
+        features = np.array(data['features']).reshape(1, -1)
+        prediction = model.predict(features)
+        return jsonify({'prediction': int(prediction[0])})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
