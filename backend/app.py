4dee527a1d087e0323fa2a677e39dc39b8708ade
nikhilgupta738 <ng929439@gmail.com>
2025-06-28 07:21:40 +0530

    file path

--- backend/app.py
+++ backend/app.py
@@ -62,5 +62,5 @@ def feedback():
         except Exception as e:
             return jsonify({'error': str(e)}), 400
     
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
